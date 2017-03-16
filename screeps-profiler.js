3b96ef0d1639283c580ad057de05d1caa72ca33a
Genhis <genhis1@gmail.com>
2017-03-16 19:47:38 +0100

    Correct an issue with Game.profiler.output() showing incorrect ticks.
    Prevously calling `Game.profiler.output()` after a profile had finished running
    would generate a report with the wrong ending tick.  This adds logic to make
    the disableTick the ending tick for the profiler report leading to accurate
    numbers.

--- screeps-profiler.js
+++ screeps-profiler.js
@@ -147,7 +147,9 @@ const Profiler = {
       return 'Profiler not active.';
     }
 
-    const elapsedTicks = Game.time - Memory.profiler.enabledTick + 1;
+    const endTick = Math.min(Memory.profiler.disableTick || Game.time, Game.time);
+    const startTick = Memory.profiler.enabledTick + 1;
+    const elapsedTicks = endTick - startTick;
     const header = 'calls\t\ttime\t\tavg\t\tfunction';
     const footer = [
       `Avg: ${(Memory.profiler.totalTime / elapsedTicks).toFixed(2)}`,
